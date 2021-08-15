0b2defedb015aabf991dc7dbac157a4590026381
Maurizio Grieco <maurizio.grieco@bluedog.it>
2021-08-15 02:35:33 +0200

    Setup project folders's architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul className="navbar-nav mr-auto">
+              <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
